18a65c9ad9d14e55da051cd1c00c0e94947cce58
Peter_Pert <petrtfx456@gmail.com>
2025-09-06 12:35:51 +0300

    просто пример

---
+++ лаба 1/FileName.cpp
@@ -0,0 +1,21 @@
+#include <iostream>  //подключаем библиотеку 
+using namespace std;
+using namespace stdl;
+int main()
+{
+	int a; //целочисленные 
+	char c = "с";  //выдает код буквы с 
+	bool b = 10; // True или False выводит 
+	double d = 3.1415; // подойдет и 3.0
+	float f = 4.0f;
+	unsigned int ui = 10;
+	long int li = 234234234234342; //позволяет делать очень большие чилса 
+	long long int lli = 234243243242324324;
+	auto aa = true;
+	// +-/*%
+	int res = 3 + 2 - 4 / 3 * 6;
+	int resl = 3 / 2;
+	res += 5;
+	std::cout << b << std::endl;
+	return 0;
+}
