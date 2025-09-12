520b9092bacf103a15d4049dbdf5af163335a94e
Peter_Pert <petrtfx456@gmail.com>
2025-09-12 20:58:35 +0300

    лаба 1

---
+++ лаба 1/лабораторная Инфа 1/задача 1/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	float a;
+	cin >> a;
+	if (a < 50000) {
+		setlocale(LC_ALL, "");
+		wcout <<L"Ваш налог:" << a * 13 / 100 << endl;
+	}
+	else if (50001 < a and a < 100000) {
+		setlocale(LC_ALL, "");
+		wcout << L"Ваш налог:" << a * 20 / 100 << endl;
+	}
+	else if (a > 100001) {
+		setlocale(LC_ALL, "");
+		wcout << L"Ваш налог:" << a * 25 / 100 << endl;
+	}
+	return 0;
+}
