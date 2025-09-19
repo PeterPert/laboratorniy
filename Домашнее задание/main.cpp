9a022787ed40d4b99170549441308cceacc92030
Peter_Pert <petrtfx456@gmail.com>
2025-09-19 15:03:27 +0300

    .

---
+++ Домашнее задание/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a; //ввод числа 
+	int b;
+	b = a / 100; 
+	setlocale(LC_ALL, "");
+	wcout << b << L"метра" << "  ";
+	int c;
+	c = a - b * 100;
+	setlocale(LC_ALL, "");
+	wcout << c << L"сантиметра" << endl;
+}
