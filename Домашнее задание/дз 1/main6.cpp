54b75f1c3eaaff8afde39cf675d79bb2477fca9f
Peter_Pert <petrtfx456@gmail.com>
2025-09-20 00:46:32 +0300

    дз

---
+++ Домашнее задание/дз 1/main6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a; 
+	if (a < 18) {
+		cout << "вам меньше 18 лет" << endl;
+	}
+	else if (a > 18 and a < 60) {
+		cout << "вам от 18 до 60 лет" << endl;
+	}
+	else {
+		cout << "вам больше 60 лет" << endl;
+	}
+}
