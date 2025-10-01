ac120916c9f4c4764e25bf9254293249395910c3
Peter_Pert <petrtfx456@gmail.com>
2025-10-02 01:05:19 +0300

    дз геометрич

---
+++ Домашнее задание/дз 2/геометрические/main5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int const n = 3;
+    int a[n][2];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> a[i][j];
+        }
+    }
+    double d;
+    if ((a[1][0] - a[0][0]) * (a[2][1] - a[0][1]) - (a[1][1] - a[0][1]) * (a[2][0] - a[0][0]) == 0) {
+        cout << "yes" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
