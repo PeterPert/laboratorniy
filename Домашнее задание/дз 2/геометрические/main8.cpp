3b7ed8086a75fddae4164d05ec688a925ca4c7f0
Peter_Pert <petrtfx456@gmail.com>
2025-10-03 22:04:49 +0300

    дз гео

---
+++ Домашнее задание/дз 2/геометрические/main8.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	cout << "Введите первую координату" << endl;
+	int x1, y1;
+	int x2, y2;
+	cin >> x1 >> y1;
+	cin >> x2 >> y2;
+	int dx = x1 - x2;
+	int dy = y1 - y2;
+	double d = sqrt(pow(dx, 2) + pow(dy, 2));
+	cout << d << endl;
+}
