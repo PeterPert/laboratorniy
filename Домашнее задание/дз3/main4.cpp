2a74961df015183cff7b81011f4cace50d7db002
Peter_Pert <petrtfx456@gmail.com>
2025-10-11 13:06:36 +0300

    номер 4

---
+++ Домашнее задание/дз3/main4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    // Ввод размера матрицы
+    int const m = 5;
+    int matrix[m][m];
+    int h = 0;
+    for (int j = 0; j < m / 2; j++) {
+        for (int i = h; i < m - h; i++) {
+            cin >> matrix[h][i];
+        }
+        for (int i = h + 1; i < m - h; i++) {
+            cin >> matrix[i][m - 1 - h];
+        }
+        for (int i = h + 1; i < m - h; i++) {
+            cin >> matrix[m - 1 - h][m - i - 1];
+        }
+        for (int i = h + 1; i < m - h - 1; i++) {
+            cin >> matrix[m - i - 1][m - (m - h)];
+        }
+        h++;
+    }
+    if (m % 2 != 0) {
+        cin >> matrix[m / 2][m / 2];
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
